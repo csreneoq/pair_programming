d58c7c1de872b828a51300bfa5c77f9a736433e7
csreneoq <csreneoq@gmail.com>
2016-11-14 14:30:38 -0300

    first solution

--- js/hello_world/lib/hello.js
+++ js/hello_world/lib/hello.js
@@ -1,5 +1,13 @@
 // Creates object and assigns it to Hello.
-var Hello = {}
+var Hello = {
+
+	helloWorld:function(name){
+		var start = "hello ";
+		var end = name == undefined? "world": name;
+		var answer = start.concat(end);
+		return answer;
+	}
+}
 
 // Your code goes here.
 

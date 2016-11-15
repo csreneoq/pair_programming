e72e455f0141e4dc70b367d165c25dc8438c70b2
csreneoq <csreneoq@gmail.com>
2016-11-15 13:21:12 -0300

    solucao funcional, implementando um filtro

--- js/even_array/lib/even_array.js
+++ js/even_array/lib/even_array.js
@@ -1,15 +1,30 @@
 function EvenArray(array = []){
    var answer = [];
 	if (array.length!=0){
-	
-	    for (var i =0; i< array.length ;i++){
-	        if (array[i]%2 == 0) {
-	           answer.push(array[i]);
-	        }
-	    }
-	}
-	
+	   answer = myFilter(array,isEven);	    	
+	}	
 	return answer;
+}
+
+function isEven(n){
+	return n %2 ==0;
+}
+
+function myFilter(inArray = [], conditionIsTrue) {
+
+    var head = inArray[0];    
+
+	if (head == undefined) {
+		return [];
+	}else 
+
+    inArray.shift();
+
+    if (!conditionIsTrue(head)){
+		return myFilter(inArray,conditionIsTrue);
+	}else
+
+    return [head].concat(myFilter(inArray, conditionIsTrue));
 	
 }
 
